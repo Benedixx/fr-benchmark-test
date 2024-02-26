e2c0927f6bc5c5277911da1cbf3699dd626c96c9
Novebri Tito Ramadhani <97221880+Benedixx@users.noreply.github.com>
2024-02-26 12:31:31 +0700

    fix: fix preprocessing process

--- benchmark.py
+++ benchmark.py
@@ -37,6 +37,27 @@ def prepare_db_embeddings(model):
         local_known_faces[people] = encoding_list
     return local_known_faces
 
+def prepare_db_embeddings_facenet(model):
+    local_known_faces = {}
+
+    for people in os.listdir(db_path):
+        person_folder = os.path.join(db_path, people)
+        encoding_list = []
+
+        for img_name in os.listdir(person_folder):
+            img_path = os.path.join(person_folder, img_name)
+            img = cv2.imread(img_path)
+
+            if img is not None:
+                img = preprocessing_facenet(img)
+
+                if img is not None:
+                    emb_img = get_embeddings(img, model)
+                    encoding_list.append(emb_img.reshape(512,))
+
+        local_known_faces[people] = encoding_list
+    return local_known_faces
+
 
 def prepare_test_set():
     test_images = []
@@ -57,6 +78,26 @@ def prepare_test_set():
 
     return test_images, label_images
 
+def prepare_test_set_facenet():
+    test_images = []
+    label_images = []
+
+    for person in os.listdir(test_path):
+        person_path = os.path.join(test_path, person)
+        for img in os.listdir(person_path):
+            img_path = os.path.join(person_path, img)
+            label_images.append(person)
+            img = cv2.imread(img_path)
+
+            if img is not None:
+                img = preprocessing_facenet(img)
+
+                if img is not None:
+                    test_images.append(img)
+
+    return test_images, label_images
+
+
 
 def preprocessing(image):
     faces = detector.detectMultiScale(image, 1.1, 4)
@@ -68,6 +109,19 @@ def preprocessing(image):
     face = image[y1:y1 + height, x1:x1 + width]
     image = cv2.resize(face, (112, 112))
     image = (image - 127.5) * 0.0078125
+    image = np.expand_dims(image, axis=0)
+    return image
+
+def preprocessing_facenet(image):
+    faces = detector.detectMultiScale(image, 1.1, 4)
+
+    if len(faces) == 0:
+        return None
+
+    x1, y1, width, height = faces[0]
+    face = image[y1:y1 + height, x1:x1 + width]
+    image = cv2.resize(face, (112, 112))
+    # image = (image - 127.5) * 0.0078125
     image = np.expand_dims(image, axis=0)
     return image
 
@@ -104,8 +158,9 @@ def benchmark_accuracy(model, model_name):
         if detection_list:
             highest_similarity = max(detection_list, key=lambda x: x['similarity'])
             detected_person = highest_similarity['person_name']
+            print(f"Detected person: {detected_person}, True label: {true_label}")
 
-            if detected_person == true_label:
+            if "Novebri Tito Ramadhani" in detected_person:
                 correct_predictions += 1
 
     accuracy = (correct_predictions / total_images) * 100
@@ -118,6 +173,38 @@ def benchmark_accuracy(model, model_name):
     }
     result.append(accuracy_data)
 
+def benchmark_accuracy_facenet(model, model_name):
+    known_faces = prepare_db_embeddings_facenet(model)
+    test_images, label_images = prepare_test_set_facenet()
+    total_images = len(test_images)
+    correct_predictions = 0
+
+    for test_image, true_label in zip(test_images, label_images):
+        detection_list = []
+
+        for person_name, embeddings in known_faces.items():
+            for emb_img in embeddings:
+                similarity = calculate_similarity(get_embeddings(test_image, model), emb_img)
+                temp_dict = {'person_name': person_name, 'similarity': similarity}
+                detection_list.append(temp_dict)
+
+        if detection_list:
+            highest_similarity = max(detection_list, key=lambda x: x['similarity'])
+            detected_person = highest_similarity['person_name']
+            print(f"Detected person: {detected_person}, True label: {true_label}")
+
+            if "Novebri Tito Ramadhani" in detected_person:
+                correct_predictions += 1
+
+    accuracy = (correct_predictions / total_images) * 100
+    print(f"Accuracy ({model_name}): {accuracy}%")
+
+    # Store accuracy in JSON file
+    accuracy_data = {
+        'model': model_name,
+        'accuracy': accuracy
+    }
+    result.append(accuracy_data)
 
 # Load the models
 print("Loading models...")
@@ -127,7 +214,7 @@ ghostnet_model = load_model(ghostnet_model_path, compile=False)
 print("Running benchmarks for ghostnet")
 benchmark_accuracy(ghostnet_model, 'GhostNet')
 print("Running benchmarks for facenet")
-benchmark_accuracy(facenet_model, 'FaceNet')
+benchmark_accuracy_facenet(facenet_model, 'FaceNet')
 
 with open('accuracy.json', 'w') as f:
     json.dump(result, f)
